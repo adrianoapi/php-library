92c8a59219e050f73e28915deeef1bb620d06772
adrianometalslug <sdcomputadores@gmail.com>
2017-10-10 16:19:12 -0300

    corrigindo o exemplo namespaces

--- namespace1/c.php
+++ namespace1/c.php
@@ -4,4 +4,11 @@ require_once 'a.php';
 use Application\Form;
 
 var_dump(new Form);
+// object(Application\Form)#1 (0) { }
+
+
 #var_dump(new Field); Erro: Use não defnido
+
+# Correção
+var_dump(new \Application\Field());
+// object(Application\Field)#1 (0) { }
