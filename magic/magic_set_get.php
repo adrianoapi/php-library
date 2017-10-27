0c8d0dd0316aed10ac7466155eee18363c8f2c56
adrianometalslug <sdcomputadores@gmail.com>
2017-10-27 14:22:15 -0200

    adicionando try catch e corrigindo getValor() em magic __get __set

--- magic/magic_set_get.php
+++ magic/magic_set_get.php
@@ -16,7 +16,7 @@ class Titulo
 
     public function getValor()
     {
-        $vecto = new DateTime($this->data[$dt_vencimento]);
+        $vecto = new DateTime($this->data['dt_vencimento']);
         $agora = new DateTime('now');
         if ($vecto < $agora) {
             $interval = $vecto->diff($agora);
@@ -36,9 +36,27 @@ class Titulo
         }
     }
 
-    public function setVencimento()
+    public function setVencimento($vencimento)
     {
-        
+        $partes = explode('-', $vencimento);
+        if (count($partes) == 3) {
+            if (checkdate($partes[1], $partes[2], $partes[0])) {
+                $this->data['dt_vencimento'] = $vencimento;
+            } else {
+                throw new Exception("Data {$vencimento} inválida");
+            }
+        }
     }
 
+}
+
+try {
+    $titulo = new Titulo;
+    $titulo->dt_vencimento = '2017-05-20';
+    $titulo->valor = 12345;
+    $titulo->multa = 2;
+    $titulo->juros = 0.01;
+    print 'O valor é: ' . number_format($titulo->valor, 2, ',', '.');
+} catch (Exception $ex) {
+    print $ex->getMessage();
 }
