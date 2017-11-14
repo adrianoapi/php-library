43f01ebbf7beb946585a81ba0fae2b011f5be71b
Adriano A Costa <sdcomputadores@gmail.com>
2017-11-14 20:11:00 -0200

    função para gerar números aleatórios de 1 a 60 sem repetição

---
+++ others/randon.php
@@ -0,0 +1,16 @@
+<?php
+
+function radon_numbers()
+{
+    $data = array();
+    while (count($data) < 6) {
+        $r = rand(1, 60);
+        if (!in_array($r, $data)) {
+            array_push($data, $r);
+        }
+    }
+    return $data;
+}
+echo "<pre>";
+print_r(radon_numbers());
+echo "</pre>";
